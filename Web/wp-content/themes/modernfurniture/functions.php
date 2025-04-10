c8709013d943102fcfd1bb7fecc2b6543bec55b1
StanleyN0vak <darkmarek2@gmail.com>
2025-04-10 19:13:22 +0200

    Add bootstrap and setup jQuery

--- Web/wp-content/themes/modernfurniture/functions.php
+++ Web/wp-content/themes/modernfurniture/functions.php
@@ -4,3 +4,8 @@ function modern_furniture_enqueue_styles() {
     wp_enqueue_style('modern-furniture', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'modern_furniture_enqueue_styles');
+
+function modern_furniture_enqueue_scripts() {
+    wp_enqueue_script('jquery');
+}
+add_action('wp_enqueue_scripts', 'modern_furniture_enqueue_scripts');
