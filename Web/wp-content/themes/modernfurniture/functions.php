9c5e90ef0b620837ef4cb2422ef830d9fd528b95
StanleyN0vak <darkmarek2@gmail.com>
2025-04-12 09:29:57 +0200

    Add img for hero; Add hero for main page

--- Web/wp-content/themes/modernfurniture/functions.php
+++ Web/wp-content/themes/modernfurniture/functions.php
@@ -9,3 +9,9 @@ function modern_furniture_enqueue_scripts() {
     wp_enqueue_script('jquery');
 }
 add_action('wp_enqueue_scripts', 'modern_furniture_enqueue_scripts');
+
+function add_svg_to_upload_mimes($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'add_svg_to_upload_mimes');
